b59a1c2b2cf32fc499f73f4e0877ac7897399b5d
Santosh <santosh.787402@smc.tu.edu.np>
2025-01-17 20:52:44 +0545

    added delete functionaltiy to delete single data from cart item edit and other function will be done in upcoming events

--- controller/user/cart/cartController.js
+++ controller/user/cart/cartController.js
@@ -28,12 +28,112 @@ exports.addToCart = async (req, res) => {
     }
 };
 
+//getting my cart items
+exports.getCartItems = async (req, res) => {
+    try {
+        const userID = req.user.id;
+        // get specific user
+        const user = await User.findById(userID);
+        if (!user) {
+            return res.status(404).json({ msg: "User not found" });
+        }
+
+        //this code will display user and nested cart having id will be populated
+        //as schema is already defined with product name 
+        //it will refer to product collection and join the product basically gives
+        //all details of product respect to id
+        const cartData = await user.populate({
+            path: "cart",
+            // sleective election deducting only product  status hence status is not shown 
+            select: "-productStatus"
+        })
+        res.status(200).json({
+            message: "fetched carted items",
+            //this is whole nestd data to limit data we only take nested part alled cart
+            data: cartData.cart
+        })
+
+        //another way
+        // inside that user there is nested cart use that cart that has id
+        //then use that id to find the actual product 
+        // const cartItems = await Product.find({ _id: { $in: user.cart } });
+        // res.status(200).json(cartItems);
+
+
+    } catch (error) {
+        res.status(500).json({ msg: "Server error", error: error.message });
+    }
+}
+
+
 
 
 // edit the cart functionality
 
 
 
+//removing element from cart
+exports.removeCartItem = async (req, res) => {
+    try {
+        // Retrieve user ID from token
+        const userID = req.user.id;
+        // Get cart ID from URL parameter
+        const cartID = req.params.cartID;
+        // Get specific user
+        const user = await User.findById(userID);
+        const product = await Product.findById(cartID);
+        
+        if (!cartID) {
+            return res.status(404).json({ msg: "Cart not found" });
+        }
+        if (!user) {
+            return res.status(404).json({ msg: "User not found" });
+        }
+        if (!product) {
+            return res.status(404).json({ msg: "Product not found" });
+        }
+        
+        // remove product from cart
+        //bsically filter loops inside the cart x being iteration
+        //only selects item thatis not cartid
+        //basically id is in object form we check it with the cart id string by conveting it to stirng
+        // should not use parenthesis in nested function of filter
+        //ot it will create error and deletye all the items
+        user.cart = user.cart.filter((x) => x.toString() !== cartID);
+        await user.save();
+        
+        res.status(200).json({
+            message: "Removed an item from cart"
+        });
+
+    } catch (error) {
+        res.status(500).json({ msg: "Server error", error: error.message });
+    }
+};
 
 
-// remove from cart functionality
+
+// exports.removeCartItem = async (req, res) => {
+//     try {
+//         // retrive user id from token
+//         const userID = req.user.id;
+//         // get cart id from url parameter
+//         const cartID = req.params.id;
+//         // get specific user
+//         const user = await User.findById(userID);
+//         if (!user) {
+//             return res.status(404).json({ msg: "User not found" });
+//         }
+//         // find the cart item and delete it
+//         const cartItem = await Cart.findByIdAndDelete(cartID);
+//         if (!cartItem) {
+//             return res.status(404).json({ msg: "Cart item not found" });
+//         }
+//         // update the user's cart
+//         user.cart = user.cart.filter((item) => item.toString() !== cartID);
+//         await user.save();
+//         res.status(200).json({ msg: "Cart item removed" });
+//     } catch (error) {
+//         res.status(500).json({ msg: "Server error", error: error.message });
+//     }
+// }
--- routes/user/cartRoute.js
+++ routes/user/cartRoute.js
@@ -1,22 +1,28 @@
-// step 3: including the file where the functionality can be found and models
-//if necessary so we can dleegate task to those file 
+    // step 3: including the file where the functionality can be found and models
+    //if necessary so we can dleegate task to those file 
 
-const { addToCart } = require("../../controller/user/cart/cartController")
-const isUserAuthenticated = require("../../middlewares/isAuthenticated")
-const catchAsync = require("../../services/catchAsync")
-
-
-// step 1: including router to be exported
-// the roter is a function dont forget it using Router will only incur error that is not there 
-//this is nasty thing to forget
-const rtr = require("express").Router()
-
-//step 4: which endpoint what to execute there
-rtr.route("/cart/:productID").post(isUserAuthenticated, catchAsync(addToCart))
-
-//strp 2: exportng the router to use by other files
-module.exports = rtr
+    const { addToCart, getCartItems, removeCartItem } = require("../../controller/user/cart/cartController")
+    const isUserAuthenticated = require("../../middlewares/isAuthenticated")
+    const catchAsync = require("../../services/catchAsync")
 
 
-//step 5 and 6: final step is to make a variable of this whole route in the maimn app.js file see there
-//next step is to use that route there at the app,js so we can seethere 
+    // step 1: including router to be exported
+    // the roter is a function dont forget it using Router will only incur error that is not there 
+    //this is nasty thing to forget
+    const rtr = require("express").Router()
+
+    //getting carted items
+    rtr.route("/cart").get(isUserAuthenticated, catchAsync(getCartItems))
+    //step 4: which endpoint what to execute there
+    rtr.route("/cart/:productID")
+    .post(isUserAuthenticated, catchAsync(addToCart))
+
+    rtr.route("/cart/:cartID")
+    .delete(isUserAuthenticated, catchAsync(removeCartItem))
+
+    //strp 2: exportng the router to use by other files
+    module.exports = rtr
+
+
+    //step 5 and 6: final step is to make a variable of this whole route in the maimn app.js file see there
+    //next step is to use that route there at the app,js so we can seethere 
